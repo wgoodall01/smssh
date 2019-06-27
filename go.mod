2c7e6996631ebf4c3c13b64df4808ba1124a0603
William Goodall <wgoodall01@gmail.com>
2019-06-27 17:12:42 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/wgoodall01/smssh
+
+go 1.12
+
+require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/inconshreveable/log15 v0.0.0-20180818164646-67afb5ed74ec // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/kevinburke/go-types v0.0.0-20181103050146-31250f49d75a // indirect
+	github.com/kevinburke/go.uuid v1.2.0 // indirect
+	github.com/kevinburke/rest v0.0.0-20190424050029-77c0c82d0ded // indirect
+	github.com/kevinburke/twilio-go v0.0.0-20190528005752-1687daad4e32
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/ttacon/builder v0.0.0-20170518171403-c099f663e1c2 // indirect
+	github.com/ttacon/libphonenumber v1.0.1 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce // indirect
+)
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/kevinburke/twilio-go"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+const inFileName string = "in.fifo"
+const outFileName string = "out.fifo"
+
+func fatal(msg string, err error) {
+	if err != nil {
+		log.Fatalf("%s: %v", msg, err)
+	}
+}
+
+var TwilioHostUrl string
+var TwilioAccountSid string
+var TwilioAuthToken string
+var TwilioPhoneNumber string
+var UserPhoneNumber string
+var ListenAddr string
+
+func main() {
+	// Note: TWILIO_HOST_URL should NOT include "/twilio" at the end of the URL.
+	// Example: "https://some.domain.here.com", "https://something.ngrok.io"
+	TwilioHostUrl = os.Getenv("TWILIO_HOST_URL")
+	TwilioAccountSid = os.Getenv("TWILIO_ACCOUNT_SID")
+	TwilioAuthToken = os.Getenv("TWILIO_AUTH_TOKEN")
+	TwilioPhoneNumber = os.Getenv("TWILIO_PHONE_NUMBER")
+	UserPhoneNumber = os.Getenv("USER_PHONE_NUMBER")
+	ListenAddr = fmt.Sprintf(":%s", os.Getenv("PORT"))
+
+	// Initialize the twilio client
+	client := twilio.NewClient(TwilioAccountSid, TwilioAuthToken, nil)
+
+	// Process stdio pipes
+	readStdin, writeStdin := io.Pipe()
+	readStdout, writeStdout := io.Pipe()
+
+	// Put lines from SMS callbacks onto stdin
+	http.HandleFunc("/twilio", func(w http.ResponseWriter, r *http.Request) {
+		err := twilio.ValidateIncomingRequest(TwilioHostUrl, TwilioAuthToken, r)
+		if err != nil {
+			log.Println("Got invalid Twilio webhook request:", err)
+			w.WriteHeader(500)
+			return
+		}
+
+		if from := r.Form.Get("From"); from != UserPhoneNumber {
+			log.Println("ignoring message from", from)
+			return
+		}
+
+		// Write the body to stdin
+		body := r.Form.Get("Body")
+		log.Println("<<", body)
+		if !strings.HasSuffix(body, "\n") {
+			body = body + "\n"
+		}
+		io.WriteString(writeStdin, body)
+
+		w.WriteHeader(200)
+	})
+
+	// Read lines from stdout, sending them as 140-character SMS messages.
+	go func() {
+		scanner := bufio.NewScanner(readStdout)
+		for scanner.Scan() {
+			line := scanner.Text()
+			log.Println(">>", line)
+			client.Messages.SendMessage(TwilioPhoneNumber, UserPhoneNumber, line, nil)
+		}
+	}()
+
+	// Run the shell, restarting it if it closes.
+	go func() {
+		for {
+			// Open a shell
+			log.Println("starting shell process...")
+			shell := exec.Command("/usr/bin/env", "bash")
+			shell.Stdout = writeStdout
+			shell.Stderr = writeStdout
+			shell.Stdin = readStdin
+			err := shell.Start()
+			fatal("shell run failed", err)
+			log.Println("shell started")
+
+			err = shell.Wait()
+			log.Println("shell stopped")
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				// Just an exit code, continue.
+				log.Printf("shell exited: %v\n", exitErr)
+			} else {
+				// Fail if it's something else
+				fatal("shell exit failed", err)
+			}
+		}
+	}()
+
+	log.Println("Listening")
+	http.ListenAndServe(":8080", nil)
+}
